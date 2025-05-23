6f3213e160f22afe843ea5bddbd28799f6004e89
Thanh Tinh Nguyen <ngthanhtinh801@gmail.com>
2025-05-23 12:18:38 +0700

    refactor DAO

--- src/java/Model/MaGiamGiaDAO.java
+++ src/java/Model/MaGiamGiaDAO.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
  * @author Windows 10
  */
 public class MaGiamGiaDAO {
-    
+
     public MaGiamGiaQLBX createMaGiamGia(ResultSet rs) throws Exception {
         MaGiamGiaQLBX maGiamGia = new MaGiamGiaQLBX(
                 rs.getInt("IdMaGiamGia"),
@@ -26,10 +26,9 @@ public class MaGiamGiaDAO {
                 rs.getInt("TrangThai")
         );
 
-
         return maGiamGia;
     }
-    
+
     public MaGiamGiaQLBX getByIdMaGiamGia(int idMaGiamGia) throws Exception {
         MaGiamGiaQLBX maGiamGia = null;
         Connection conn = DBConnection.getConnection();
@@ -46,5 +45,37 @@ public class MaGiamGiaDAO {
         ps.close();
         conn.close();
         return maGiamGia;
+    }
+
+    public Integer getIdMaGiamGia(String maGiamGia) throws Exception {
+        Integer idMaGiamGia = null;
+        Connection conn = DBConnection.getConnection();
+        String sql = "SELECT IDMaGiamGia FROM MaGiamGia WHERE MaGiamGia = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, maGiamGia);
+
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            idMaGiamGia = rs.getInt("IDMaGiamGia");
+        }
+
+        rs.close();
+        ps.close();
+        conn.close();
+        return idMaGiamGia;
+    }
+
+    public void disableMaGiamGia(String maGiamGia) throws Exception {
+        Connection conn = DBConnection.getConnection();
+        String sql = "UPDATE MaGiamGia "
+                + "SET TrangThai = ? "
+                + "WHERE MaGiamGia = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setInt(1, 0);
+        ps.setString(2, maGiamGia);
+        
+        ps.executeUpdate();
+        ps.close();
+        conn.close();
     }
 }
